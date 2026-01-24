3d545d315a7baf6f20c23631195acb692f9a5214
Santhosh S <valarmathisanthosh33@gmail.com>
2026-01-24 16:59:07 +0530

    Fix mobile login/register + add type module

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -29,7 +29,7 @@ app.use("/api/orders", orderRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get(/.*/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
