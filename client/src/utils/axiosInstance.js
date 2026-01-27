953d68f686fe87add714defe6e71c5cef92c558b
Santhosh S <valarmathisanthosh33@gmail.com>
2026-01-27 20:53:05 +0530

    fix: update axios baseURL to use VITE_BASE_URL for backend API

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://sandy-sweet-nest-2.onrender.com',
-   withCredentials: true,
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true, // 🔥 THIS WAS MISSING
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Add interceptor to include Authorization header
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -17,9 +16,7 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
